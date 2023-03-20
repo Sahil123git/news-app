60cac691deef77f3c6e81a7f14e847872c046f6c
Sahil Dussa <sahildussa457@gmail.com>
2023-03-20 10:27:47 +0530

    addApiContent

---
+++ Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h1 className="bg-dark text-light text-center p-1">Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="bg-dark text-light text-center p-1">Header </h1>
+    </div>
+  );
+};
+
+export default Header;
